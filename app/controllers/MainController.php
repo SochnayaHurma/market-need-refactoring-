ac48dcd2811178fb265ac909a1eb98ede59cbd28
Ben <dia_v3@rambler.ru>
2023-07-14 22:13:11 +0300

    Added class Cache

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -4,6 +4,7 @@ namespace app\controllers;
 
 use RedBeanPHP\R;
 use dopler_core\App;
+use dopler_core\Cache;
 
 
 class MainController extends AppController
@@ -16,7 +17,6 @@ class MainController extends AppController
         $products = $this->model->get_hits($current_language['id'], 6); 
         $this->set(compact('slides', 'products'));
         $this->setMeta(___("main_index_meta_title"), ___("main_index_meta_description"), ___("main_index_meta_keywords"));
-
     }
 }
 ?>
---
+++ vendor/dopler_core/Cache.php
@@ -0,0 +1,43 @@
+<?php 
+namespace dopler_core;
+
+class Cache
+{
+    use TSingleton;
+
+    public function set(string $key, string|array|object $data, int $seconds = 3600): bool
+    {
+        $content['data'] = $data;
+        $content['end_time'] = time() + $seconds;
+
+        if (file_put_contents(CACHE . '/' . md5($key) . '.txt', serialize($content))) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function get(string $key)
+    {
+        $file = CACHE . '/' . md5($key) . '.txt';
+        if (file_exists($file)) {
+            $content = unserialize(file_get_contents($file));
+            if (time() <= $content['end_time']) {
+                return $content['data'];
+            }
+            unlink($file);
+        } else { 
+            return false;
+        }
+    }
+
+    public function delete(string $key): void
+    {
+        $file = CACHE . '/' . md5($key) . '.txt';
+        if (file_exists($file)) {
+            unlink($key);
+        }
+    }
+}
+
+?>
